c495d546d6bb1667e94f933205b17ac386c59c42
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2023-08-23 12:14:10 +0200

    Add comments

--- ROS_Plugins/catkin_ws/src/robot_control/nodes/pid_controller.py
+++ ROS_Plugins/catkin_ws/src/robot_control/nodes/pid_controller.py
@@ -16,7 +16,7 @@ Kp = 1.5
 Kd = 0.001
 Ki = 0.025
 
-threshold = 3.5        # acceptable error range for the robot to continue in a straight direction
+threshold = 3.5      # acceptable error range for the robot to continue in a straight direction
 speed_threshold = 8  # maximum amount of speed generated the correction of the cruise
 K_speed = 0.1        # attenuation parameter for the speed imposed to wheels
 
@@ -25,65 +25,73 @@ wheel_srv = rospy.ServiceProxy('/husky_model/husky/wheel_speeds_service', SetHus
 is_reset = False
 flip = True
 
-areas = np.array([0])
-prev_time = 0
-prev_err = 0
+areas = np.array([0]) # array containing the rectangular areas subtended by the error function for calculating the integral (sum of areas)
+prev_time = 0         # last (previous) moment at which a correction was computed
+prev_err = 0          # last (previous) error on which a correction was computed
 
+# method to reset the state of the PID controller when the target is changed
 def reset():
     global areas, prev_time, prev_err, is_reset
     prev_time = prev_err = 0
     areas = np.array([0])
     is_reset = True
 
+# method containing the PID controller algorithm
 def pid(vision):
     global areas, prev_time, prev_err, is_reset, flip
-    if (vision.target_found):
+
+    if (vision.target_found): # case: a human is recongized by Yolov8 in the camera field of view
         is_reset = False
-        diff_x = vision.center_x - vision.target_x
-        print("DIFF: ", diff_x)
+
+        # the PID controller base its correction on the difference between the center of the camera field of view and the center of the identified human
+        # in this way the robot aims directly to the identified human
+        diff_x = vision.center_x - vision.target_x 
         t = time.time()
         areas = np.append(areas, diff_x * (t - prev_time))
         
         proportional = Kp * diff_x
-        derivative = Kd * (diff_x - prev_err) / (t - prev_time)
-        integral = Ki * sum(areas[2:])
+        derivative = Kd * (diff_x - prev_err) / (t - prev_time) # application of the derivative formula to this specific instant
+        integral = Ki * sum(areas[2:]) # calculated as the sum of the areas subtended by the error function
         
         prev_err = diff_x
         prev_time = t
         result = proportional + derivative + integral
         abs_result = abs(result)
         final_speed = abs_result
-        print("RESULT: ", result)
+        
+        # the maximum velocity is limited by a threshold so to better apply in the specific case of this project
         if (abs_result > speed_threshold):
             final_speed = speed_threshold
 
-        #back_left, back_right, front_left, front_right
-        w_speed = WheelSpeeds(0, 0, 0, 0)
-        if (result >= threshold): #vai a sinistra
-            print("LEFT")
+        # as the correction has been computed a speed is applied to the wheels thanks to the specific ROS service    
+        w_speed = WheelSpeeds(0, 0, 0, 0) # back_left, back_right, front_left, front_right
+        if (result >= threshold): # turn left
             w_speed.back_right_wheel = K_speed * final_speed
             w_speed.front_right_wheel = K_speed * final_speed
-        elif (result <= -threshold): #vai a destra
-            print("RIGHT")
+
+        elif (result <= -threshold): # turn right
             w_speed.back_left_wheel = K_speed * final_speed
             w_speed.front_left_wheel = K_speed * final_speed
-        else: #vai dritto
-            print("STRAIGHT")
+
+        else: # go straight
             w_speed.back_left_wheel = 5
             w_speed.back_right_wheel = 5
             w_speed.front_left_wheel = 5
             w_speed.front_right_wheel = 5
             
         wheel_srv(w_speed)
+
     else:
         if (not is_reset):
             reset()
         
         w_speed = WheelSpeeds(0, 0, 0, 0)
         
-        if flip:
+        # when Yolov8 can't perceive a human in the camera field of view, the robot will move in order to rotate anti-clockwise until a new human is found
+        if flip: 
             w_speed.back_right_wheel = 3
             w_speed.front_right_wheel = 3
+
         else:
             w_speed.back_left_wheel = -0.5
             w_speed.back_right_wheel = -0.5
@@ -93,8 +101,8 @@ def pid(vision):
         wheel_srv(w_speed)
         flip = not flip
 
-
-    
+# ROS node initialization method 
+# register and listen on the "/vision" topic to receive information about the target (Vision ROS msg) from the "v_talker" ROS node
 def wheel_controller():
     rospy.init_node("r_controller")
     rospy.Subscriber("/vision", Vision, pid, queue_size=1)
